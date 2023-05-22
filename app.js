c43ffecd93e2f81021e84ea91b8e1728628f524d
SirNando <matiasnandin@hotmail.com>
2023-05-22 09:46:32 -0300

    Updated files

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
+const mongoDbStore = require('connect-mongodb-session')(session);
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
 const app = express();
 
+const sessionStore = new mongoDbStore({
+  uri: 'mongodb://127.0.0.1:27017/auth-demo',
+  collection: 'sessions'
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'super-secret-key-ngl',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
 
 app.use(demoRoutes);
 
--- routes/demo.js
+++ routes/demo.js
@@ -1,43 +1,92 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/database');
+const bcrypt = require("bcryptjs");
+
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {
+router.post("/signup", async function (req, res) {
   const userData = req.body;
   const enteredEmail = userData.email;
-  const enteredConfirmEmail = userData['confirm-email']; // Using this notation enables using dashes for the name of the field
+  const enteredConfirmEmail = userData["confirm-email"]; // Using this notation enables using dashes for the name of the field
   const enteredPassword = userData.password;
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log('Incorrect data');
+    return res.redirect('/signup');
+  }
+
+  const existingUser = await db.getDb().collection('users').findOne({email: enteredEmail});
+  if(existingUser) {
+    console.log('User exists already');
+    return res.redirect('/signup');
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
   const user = {
     email: enteredEmail,
-    password: enteredPassword
+    password: hashedPassword,
   };
 
-  await db.getDb().collection('users').insertOne(user);
+  await db.getDb().collection("users").insertOne(user);
 
-  res.redirect('/login');
+  res.redirect("/login");
 });
 
-router.post('/login', async function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log("Could not log in");
+    return res.redirect("/login");
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordsAreEqual) {
+    console.log("Could not log in (password mismatch)");
+    return res.redirect("/login");
+  }
+
+  console.log("User was authenticated");
+  res.redirect("/admin");
 });
 
-router.post('/logout', function (req, res) {});
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
